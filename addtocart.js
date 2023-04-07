942494a9315180e7430444c1d1ed5de4075eea3a
achrefbz <achrefbouzgarrou@yahoo.com>
2023-04-07 15:05:35 +0200

    all

--- addtocart.js
+++ addtocart.js
@@ -33,10 +33,13 @@ document.getElementById('root').innerHTML = categories.map((item)=>
     return(
         `<div class='box'>
             <div class='img-box'>
+            
                 <img class='images' src=${image}></img>
+                
             </div>
         <div class='bottom'>
         <p>${title}</p>
+        
         <h2>$ ${price}.00</h2>`+
         "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
         `</div>
